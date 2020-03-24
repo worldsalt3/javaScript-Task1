1fb5d8dae45348024aad62abb89fc475b2812e5d
worldsalt3 <riliwanhazzan@gmail.com>
2020-03-24 17:02:31 +0100

    first commit of the javaScript file

---
+++ script.js
@@ -0,0 +1,16 @@
+let isName = "Riliwan Hassan Adedolapo";
+let courses = ["Front end", "Coding"];
+
+console.log(`My name is ${myName}`);
+console.log(courses);
+
+const data = [];
+    if(courses.length%2 === 0){
+        for (let i=0; i <=200; i++){
+            if(i%2 === 0){
+                data.push(i);
+            }
+        }
+       console.log(data); 
+    }
+    
